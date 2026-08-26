abcd17242649ac4dde0236d49a996ca74b280e26
Pavel Matveev <pavelmatveev84@gmail.com>
2026-08-27 10:19:34 +1100

    Release OCR2Geometry v0.6.0

--- ocr_coordinates_to_geometry/dialog.py
+++ ocr_coordinates_to_geometry/dialog.py
@@ -390,7 +390,7 @@ class OcrCoordinatesDialog(QDialog):
             python_executable = str(error)
         return "\n".join(
             [
-                "OCR2Geometry: 0.6.0-beta1",
+                "OCR2Geometry: 0.6.0",
                 f"QGIS: {Qgis.QGIS_VERSION}",
                 f"Locale: {self.locale}",
                 f"OS: {platform.platform()}",
@@ -406,7 +406,7 @@ class OcrCoordinatesDialog(QDialog):
         dialog.setWindowTitle(self.tr("about_title"))
         dialog.resize(620, 430)
         layout = QVBoxLayout(dialog)
-        title = QLabel("<h2>OCR2Geometry 0.6.0-beta1</h2>")
+        title = QLabel("<h2>OCR2Geometry 0.6.0</h2>")
         title.setTextFormat(Qt.TextFormat.RichText)
         layout.addWidget(title)
         description = QLabel(
